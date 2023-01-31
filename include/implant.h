343ca6f84aae8d635560e48cfba56665fe2ac99e
g0rchy <92731777+g0rchy@users.noreply.github.com>
2023-01-31 14:08:09 +0100

    remove unused func delcarations

--- include/implant.h
+++ include/implant.h
@@ -15,9 +15,5 @@ int create_socket(void);
 int check_magic_byte(struct icmphdr *icmp);
 size_t invoke_command(unsigned char *data, unsigned char *output);
 void interact(int sockfd, char *dest_ip);
-struct sockaddr_in prep_ip_headers(struct iphdr *ip);
-unsigned char *parse_data_section(unsigned char *packet);
-void prep_icmp_headers(struct icmphdr *icmp, unsigned char *data, uint16_t checksum);
-void append_to_data_section(struct icmphdr *icmp, unsigned char *data, unsigned char *input);
 
 #endif
